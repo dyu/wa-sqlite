c97ec323563d574daa485203c5b5ba6dc6d26510
dyu <david.yu.ftw@gmail.com>
2023-03-23 05:54:13 +0800

    ns updates

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1012,7 +1012,7 @@ declare interface SQLiteAPI {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/sqlite-constants.js' {
+declare module '@dyu/wa-sqlite/src/sqlite-constants.js' {
   export const SQLITE_OK: 0;
   export const SQLITE_ERROR: 1;
   export const SQLITE_INTERNAL: 2;
@@ -1194,8 +1194,8 @@ declare module '@vlcn.io/wa-sqlite/src/sqlite-constants.js' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite' {
-  export * from '@vlcn.io/wa-sqlite/src/sqlite-constants.js';
+declare module '@dyu/wa-sqlite' {
+  export * from '@dyu/wa-sqlite/src/sqlite-constants.js';
 
   /**
    * Builds a Javascript API from the Emscripten module. This API is still
@@ -1213,20 +1213,20 @@ declare module '@vlcn.io/wa-sqlite' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/dist/wa-sqlite.mjs' {
+declare module '@dyu/wa-sqlite/dist/wa-sqlite.mjs' {
   function ModuleFactory(config?: object): Promise<any>;
   export = ModuleFactory;
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/dist/wa-sqlite-async.mjs' {
+declare module '@dyu/wa-sqlite/dist/wa-sqlite-async.mjs' {
   function ModuleFactory(config?: object): Promise<any>;
   export = ModuleFactory;
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/VFS.js' {
-  export * from '@vlcn.io/wa-sqlite/src/sqlite-constants.js';
+declare module '@dyu/wa-sqlite/src/VFS.js' {
+  export * from '@dyu/wa-sqlite/src/sqlite-constants.js';
 
   export class Base {
     mxPathName: number;
@@ -1351,7 +1351,7 @@ declare module '@vlcn.io/wa-sqlite/src/VFS.js' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/examples/ArrayModule.js' {
+declare module '@dyu/wa-sqlite/src/examples/ArrayModule.js' {
   export class ArrayModule {
     /**
      * @param {SQLiteAPI} sqlite3
@@ -1460,8 +1460,8 @@ declare module '@vlcn.io/wa-sqlite/src/examples/ArrayModule.js' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/examples/ArrayAsyncModule.js' {
-  import { ArrayModule } from "@vlcn.io/wa-sqlite/src/examples/ArrayModule.js";
+declare module '@dyu/wa-sqlite/src/examples/ArrayAsyncModule.js' {
+  import { ArrayModule } from "@dyu/wa-sqlite/src/examples/ArrayModule.js";
   export class ArrayAsyncModule extends ArrayModule {
     /**
      * @param {function} f
@@ -1472,8 +1472,8 @@ declare module '@vlcn.io/wa-sqlite/src/examples/ArrayAsyncModule.js' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/examples/IndexedDbVFS.js' {
-  import * as VFS from "@vlcn.io/wa-sqlite/src/VFS.js";
+declare module '@dyu/wa-sqlite/src/examples/IndexedDbVFS.js' {
+  import * as VFS from "@dyu/wa-sqlite/src/VFS.js";
   export class IndexedDbVFS extends VFS.Base {
     /**
      * @param {string} idbName Name of IndexedDB database.
@@ -1525,8 +1525,8 @@ declare module '@vlcn.io/wa-sqlite/src/examples/IndexedDbVFS.js' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/examples/MemoryVFS.js' {
-  import * as VFS from "@vlcn.io/wa-sqlite/src/VFS.js";
+declare module '@dyu/wa-sqlite/src/examples/MemoryVFS.js' {
+  import * as VFS from "@dyu/wa-sqlite/src/VFS.js";
   export class MemoryVFS extends VFS.Base {
     name: string;
     mapNameToFile: Map<any, any>;
@@ -1535,14 +1535,14 @@ declare module '@vlcn.io/wa-sqlite/src/examples/MemoryVFS.js' {
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/examples/MemoryAsyncVFS.js' {
-  import { MemoryVFS } from "@vlcn.io/wa-sqlite/src/examples/MemoryVFS.js";
+declare module '@dyu/wa-sqlite/src/examples/MemoryAsyncVFS.js' {
+  import { MemoryVFS } from "@dyu/wa-sqlite/src/examples/MemoryVFS.js";
   export class MemoryAsyncVFS extends MemoryVFS {
   }
 }
 
 /** @ignore */
-declare module '@vlcn.io/wa-sqlite/src/examples/tag.js' {
+declare module '@dyu/wa-sqlite/src/examples/tag.js' {
   /**
    * Template tag builder. This function creates a tag with an API and
    * database from the same module, then the tag can be used like this:
