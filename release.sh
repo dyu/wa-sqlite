5ec8d95fab6543342a98361aafca22aa5b581fb1
dyu <david.yu.ftw@gmail.com>
2024-01-10 20:44:32 +0800

    prep for release

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+VERSION=$1
+
+[ ! -n "$VERSION" ] && echo "Enter release version: " && read VERSION
+
+echo "Releasing $VERSION - are you sure? (y/n):" && read CONFIRM && [ "$CONFIRM" != "y" ] && exit 0
+
+npm version $VERSION --message "[release] $VERSION"
+
+# publish
+git push origin refs/tags/v$VERSION
+git push -u origin fts
+npm publish --access public
+echo "Released $VERSION"
